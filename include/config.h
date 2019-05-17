9ab341217ba1de3a25a775ae631a0b7ab5592de3
dawendiguo <dawendiguo88@live.com>
2019-05-17 15:41:51 +0800

    create config.h

---
+++ include/config.h
@@ -0,0 +1,258 @@
+/* config.h, Generated automatically by configure. */
+/* config.h.in, Generated automatically from configure.in bu autoheader. */
+
+/* CPU, verndor ,and operating system */
+#define CPU_VENDOR_OS  "intel-unknown-centos7.0"
+
+/* Define if <netdb.h> defines struct addrinfo */
+#define HAVE_ADDRINFO_STRUCT 1
+
+/* Define if you have the <arpa/inet.h> header file. */
+#define HAVE_APPR_INET_H 1
+
+
+/* Define if you have the bzero function .*/
+#define HAVE_BZERO 1
+
+/* Define if the /dev/streams/stiso/tcp device exists */
+/* #undef HAVE_DEV_STREAMS_XTISO_TCP */
+
+/*Define if the /dev/tcp device exists */
+/* #undef HAVA_DEV_TCP*/
+
+/*Define if you have the <errono.h> header file. */
+#define HAVE_ERRONO_H 1
+
+/*Define if you have the <fcntl.h> header file. */
+#define HAVE_FCNTL_H 1
+
+/*Define if you have the getaddrinfo function */
+#define HAVE_GETADDRINFO 1
+
+/*Define if getaddrinfo prototype is in <netdb.h> */
+#define HAVE_GETADDRINFO_PROTO 1
+
+/*Define if you have the gethostbyname2 function. */
+#define HAVE_GETHOSTBYNAME2 1
+
+/*Define if you have the gethostbyname_r function. */
+/* undef HAVE_GETHOSTBYNAME_R */
+
+/*Dfine if you have the gethostname function. */
+#define HAVE_GETHOSTNAME 1
+
+/*Define if getnameinfo prototype is in <netdb.h> */
+#define HAVE_GETHOSTNAME_PROTO 1
+
+/*Define if you have the getnameinfo function. */
+#define HAVE_GETNAMEINFO_PROTY 1
+
+/*Define if <net/if.h> defines struct if_nameindex */
+#define HAVE_IF_NAMEINDEX_STRUCT 1
+
+/*Define if you have the if_nametoindex function.*/
+#define HAVE_IF_NAMETOINDEX 1
+
+/*Define if if_nametoindex_prototype is in<net/if.h> */
+ #define HAVE_IF_NAMETOINDEX_PROTO 1
+
+/*Define if you have the inet_aton function. */
+#define HAVE_INET_ATON 1
+
+/*Define if you inet_aton prototype is in <arpa/inet.h> */
+#define HAVE_INET_ATON_PROTO 1
+
+/*Define if you have the kovent function. */
+#define HAVE_KQUEUE 1
+
+/*Dein if you have the nsl library (-lnsl). */
+/* #undef HAVE_LIBNSL */
+
+/*Define if you have the pthead library (-lpthread). */
+/* #undef HAVE_LIBPTHREAD */
+
+/*Define if you have the pthreads library (-lpthreads). */
+/* #undef HAVE_LIBPTHREADS */
+
+/*Define if you have the resolv library(-lresolv),. */
+/* #undef HAVE_LIBRESOLV */
+
+/*Define if you have the xtilibrary(-lxti).*/
+/* #undef HAVE_LIBXTI */
+
+/*Define if you have the mkstemp function. */
+#define HAVE_MKSTEMP 1
+
+/*Define if struct msghdr contains the msg_control element */
+#define HAVE_MSGHDR_MSG_CONTROL 1
+
+/*Define if you have the <netconfig.h> header file. */
+#define HAVE_NETCONFIG_H 1
+
+/*Define if you have the <netdb.h> header file. */
+#define HAVE_NETDB_H 1
+
+/*Define if you have the <netdir.h> header file. */
+/* #undef HAVE_NETDIR_H */
+
+/*Define if you have the <netinet/in.h> header file. */
+#define HAVE_NETINET_IN_H 1
+
+/*Define if you have the <net/if_dl.h> header file .*/
+#define HAVE_NET_IF_DL_H 1
+
+/*Define ifyouu have the poll function */
+#define HAVE_POLL 1
+
+/*Define if you have the <poll.h> header file. */
+#define HAVE_POLL_H 1
+
+/*Define if you have the pselect function. */
+#define HAVE_PSELECT 1
+
+/*Define if pselect prototype is in <sys/stat.h> */
+#define HAVE_PSELECT_PROTO 1
+
+/*Define if you have the <pthread.h> header file. */
+#define HAVE_PTHREAD_H 11
+
+/*Define if you have the <signal.h> header file. */
+#define HAVE_SIGNAL_H 1
+
+/*Define if you have the snprintf fuction. */
+#define HAVE_SNPRINTF_PROTO 1
+
+/*Define if <net/if_dl.h> defines struct sockaddr_dl */
+#define HAVE_SOCKADDR_DL_STRUCT 1
+
+/*Define if socket address sstructures have length fields. */
+#define HAVE_SOCKADDR_SA_LEN 1
+
+/*Define if you have the sockatmark function. */
+#define HAVE_SOCKATMARK 1
+
+/*Define if sockatmark prototype is in <sys/sockat.h>*/
+#define HAVE_SOCKATMARK_PROTO 1
+
+/*Define if you have the <stdio.h> header file. */
+#define HAVE_STDIO_H 1
+
+/*Define if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/*Define if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/*Define if you have the <stropts.h> header file */
+/* #undef HAVE_STROPTS_H 1 */
+
+/*Define if lfr_mtu is member of struct ifreg. */
+#define HAVE_STRUCT_IFREG_IFR_MTU 1
+
+/* Define if the system has the type struct sockaddr_storage. */
+#define HAVE_STRUCT_SOCKADDR_STORAGE 1
+
+/* Define if you have the <sys/event.h> header file */
+#define HAVE_SYS_EVENT_H 1
+
+/*Define if you have the <sys/filio.h> header file. */
+#define HAVE_SYS_FILIO_H 1
+
+/*Define if you have the <sys/select.h> header file. */
+#define HAVE_SYS_SELECT_H 1
+
+/*Define if you have the <sys/socket.h> header file. */
+#define HAVE_SYS_SOCKET_H 1
+
+/* Define if you have the <sys/sockio.h> header file. */
+#define HAVE_SYS_SOCKIO_H 1
+
+/*Define if you have the <sys/stat.h> header file */
+#define HAVE_SYS_STAT_H 1
+
+/*Define if you have the <syt/sysctl.h> header file */
+#define HAVE_SYS_SYSCTL_H 1
+
+/*Define if you have the <sys/time.h> header file */
+#define HAVE_SYS_TIME_H 1
+
+/*Define if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/*Define if you have the <sys/uio.h> header file. */
+#define HAVE_SYS_UIO_H 1
+
+/* Define if you have the <sys/wait.h> header file .*/
+#define HAVE_SYS_WAIT_H 1
+
+/*Define if <time.h> defines struct timespec */
+#define HAVE_TIMESPEC_STRUCT 1
+
+/*Define if you have the <time.h> header file. */
+#define HAVE_TIME_H 1
+
+/* Define if you have the <unistd.h> header file */
+#define HAVE_UNISTD_H 1
+
+/*Define if you have the vsnprint function.*/
+#define HAVE_VSNPRINTF 1
+
+/*Define if you have the <xti.h> header file. */
+/*#undef HAVE_XTI_H 1 */
+
+/*Define if you have the <xti_inet.h> header file */
+/*#undef HAVE_XTI_INET_H 1 */
+
+/*Define if the system supports IPv4 */
+#define IPV4 1
+
+/*Define if the system supports IPV6*/
+#define IPV6 1
+
+/*Define if the system supports IP Multicast */
+#define MCAST 1
+
+/*the size of the sa_family field in a socket address structure */
+/* #undef sa_family_t */
+
+/*Define if you have the ANSI C header files.*/
+#define STDC_HEADERS 1
+
+/*Define if you can safely include both <sys/time.h> and <time.h> */
+#define TIME_WITH_SYS_TIME 1
+
+/*Define if the system supports unix domain sockets */
+#define UNIXDOMAIN 1
+
+/* 16 bit signed type */
+/* #undef int16_t */
+
+/*32 bit signed type */
+/* #undef int32_t */
+
+/* the type of the sa_family struct element */
+/* #undef sa_family_t */
+
+/* unsigned integer type of the result of the sizeof operator */
+/* #undef size_t */
+
+/* define to __s_family if sockaddr_storage has thast instead of ss_family */
+/* #undef ss_family */
+
+/* a signed type approprisate for a count of bytes or an error indication */
+/* #undef ssize_t */
+
+/* scalar type */
+#define t_scalar_t int32_t
+
+/*unsigned scalar type*/
+#define t_uscalar_t uint32_t
+
+/*16 bit unsigned type */
+/* #undef uint16_t */
+
+/*32 bit unsigned type */
+/* #undef uint32_t */
+
+/* -bit unsigned type */
+/* #undef uint8_t */
